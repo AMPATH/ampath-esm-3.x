9f46008cd5621c6075fac8f1f456f2885f0ec403
Rugute <rrugute@gmail.com>
2024-05-24 18:19:09 +0300

    (chores) root.text.tsx esm-patient-registration

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
+    '@babel/preset-env', // Add this line if not already included for transpiling ES6+ features
+    '@babel/preset-react',
   ],
 };
