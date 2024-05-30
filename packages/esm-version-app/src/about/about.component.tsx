2da026dccb5d7639c6f95def01c70b0a72015377
Rugute <rrugute@gmail.com>
2024-05-30 19:56:39 +0300

    added about details

--- packages/esm-version-app/src/about/about.component.tsx
+++ packages/esm-version-app/src/about/about.component.tsx
@@ -26,14 +26,18 @@ const About: React.FC<AboutProps> = () => {
               <h3>Government of Kenya</h3>
               <h4>Ministry of Health</h4>
             </div>
-            <img
-              src="/openmrs/ms/uiframework/resource/kenyaemr/images/logos/moh.png"
-              alt="court_of_arms"
-              width="50"
-              height="50"
-            />
+            <img src="https://portal.health.go.ke/assets/coa.ec23b38a.svg" alt="court_of_arms" width="50" height="50" />
           </div>
           <div className={aboutBody}>
+            <p>AMPATH</p>
+            <p>
+              <img
+                src="https://poc-test-builds.fra1.digitaloceanspaces.com/@brand/logo_with_tagline.svg"
+                alt="court_of_arms"
+                width="150"
+                height="20"
+              />
+            </p>
             <p>AMRS Version</p>
             <p>{`v${AMRS?.version}`}</p>
             <p>SPA Version</p>
